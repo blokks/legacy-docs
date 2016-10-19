9c25e8e9dd2dcaa891d5d0fb6bfa784ea3ddc761
Patrick Pietens <hello@patrickpietens.com>
2016-10-19 13:08:04 +0200

    Server over external url

--- gulptasks/serve.js
+++ gulptasks/serve.js
@@ -14,6 +14,7 @@ gulp.task('serve', (callback) => {
     const syncOptions = {
         host: 'docs.blokks.dev',
         notify: false,
+        open: 'external',
         proxy: 'docs.blokks.dev',
         reloadDebounce: 2500,
     };

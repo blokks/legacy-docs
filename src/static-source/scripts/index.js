d9633242f20e268dc0309c8135b3721ea353ede5
Patrick Pietens <hello@patrickpietens.com>
2016-11-18 22:59:49 +0100

    Added copy to clipboard when clicking on codeblocks

--- src/static-source/scripts/index.js
+++ src/static-source/scripts/index.js
@@ -1,6 +1,9 @@
 'use strict';
 
+import Clipboard from 'clipboard';
 import { highlightBlock } from 'highlight.js';
 
 const codeBlocks = document.querySelectorAll('pre code');
 Array.from(codeBlocks).forEach((block) => highlightBlock(block));
+
+new Clipboard(codeBlocks, { target: (trigger) => trigger });

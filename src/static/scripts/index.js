fbaf0eaa74c0e698b4c6dee5c2d94116a886d369
Patrick Pietens <hello@patrickpietens.com>
2017-09-14 00:31:12 +0200

    Tweaking css

--- src/static/scripts/index.js
+++ src/static/scripts/index.js
@@ -1,20 +1,7 @@
-"use strict";
+import Clipboard from 'clipboard';
+import { highlightBlock } from 'highlight.js';
 
-import Clipboard from "clipboard";
-import { highlightBlock } from "highlight.js";
-
-const codeBlocks = document.querySelectorAll("pre code");
+const codeBlocks = document.querySelectorAll('pre code');
 Array.from(codeBlocks).forEach(block => highlightBlock(block));
 
 new Clipboard(codeBlocks, { target: trigger => trigger });
-
-const toc = document.getElementById("toc");
-const scrollTop = toc.offsetTop + toc.clientHeight - window.innerHeight + 224;
-
-window.addEventListener("scroll", event => {
-  if (document.documentElement.scrollTop >= scrollTop) {
-    toc.classList.add("fixed");
-  } else {
-    toc.classList.remove("fixed");
-  }
-});
